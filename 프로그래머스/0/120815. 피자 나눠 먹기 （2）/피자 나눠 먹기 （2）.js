dd5a73a87e6b793a0173192b5a489581f7e416ae
dev-dino22 <wlsskdud02@naver.com>
2025-03-09 17:58:05 +0900

    피자 문제 해결을 위한 코드 리팩토링 및 주석 추가

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,3 +1,6 @@
+// ㅋㅋㅋㅋㅋㅋㅋ웃긴 1차 풀이
+// 먼가 최소공배수 이용한 어쩌구일거같은데 머리 안돌아가서 냅다 피자 왕창 생성해버리기...
+
 function solution(n) {
      let tryNumber = 6;
     
@@ -11,3 +14,25 @@ function solution(n) {
     const index = setPizza.findIndex((num) => num % n === 0);
     return index + 1;
 }
+
+
+
+// 일단 리팩토링!
+// 일단 필요한만큼만 피자 만드는 걸로 하긴했는데 먼가 쫌 그른가
+
+function solution(n) {
+    let pizzaSlice = 6;
+    const setPizza = [];
+
+    while (pizzaSlice % n !== 0) {
+        setPizza.push(pizzaSlice);
+        pizzaSlice += 6;
+    }
+    setPizza.push(pizzaSlice);
+
+    return setPizza.length;
+}
+
+
+// 다른 사람들은 어떻게 풀었나?
+// "유클리드 호제법"! 학습해보고 직접 이걸 적용해 풀어보기
