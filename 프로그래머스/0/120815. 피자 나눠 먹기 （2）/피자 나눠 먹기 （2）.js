8e6cbeb639ea41d17fdb325b785160716f8042d3
dev-dino22 <141295691+dev-dino22@users.noreply.github.com>
2025-03-18 02:38:01 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,38 +1,17 @@
-// ㅋㅋㅋㅋㅋㅋㅋ웃긴 1차 풀이
-// 먼가 최소공배수 이용한 어쩌구일거같은데 머리 안돌아가서 냅다 피자 왕창 생성해버리기...
-
-function solution(n) {
-     let tryNumber = 6;
-    
-    const setPizza = [];
-    
-    for(let i = 0; i<=100; i++) {
-        setPizza.push(tryNumber);
-        tryNumber += 6;
+function gcd(a, b) {
+    let temp;
+    while (b !== 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
     }
-
-    const index = setPizza.findIndex((num) => num % n === 0);
-    return index + 1;
+   return a;
 }
 
-
-
-// 일단 리팩토링!
-// 일단 필요한만큼만 피자 만드는 걸로 하긴했는데 먼가 쫌 그른가
-
-function solution(n) {
-    let pizzaSlice = 6;
-    const setPizza = [];
-
-    while (pizzaSlice % n !== 0) {
-        setPizza.push(pizzaSlice);
-        pizzaSlice += 6;
-    }
-    setPizza.push(pizzaSlice);
-
-    return setPizza.length;
+function lmn(a, b) {
+    return a * b / gcd(a, b);
 }
 
-
-// 다른 사람들은 어떻게 풀었나?
-// "유클리드 호제법"! 학습해보고 직접 이걸 적용해 풀어보기
+function solution(n) {
+    return lmn(6, n) / 6;
+}
