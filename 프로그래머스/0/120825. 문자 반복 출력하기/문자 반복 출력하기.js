6238386764ef70e072d2b3eecb9daaf6db0b8f31
dev-dino22 <141295691+dev-dino22@users.noreply.github.com>
2025-03-24 04:57:04 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,5 +1,3 @@
 function solution(my_string, n) {
-    const splitedString = my_string.split('');
-    const answer = splitedString.map((string) => string.repeat(n)).join('');
-    return answer;
+    return my_string.split('').map((string) => string.repeat(n)).join('');
 }
